d67ee6dd6d016db7907337c35aefb58c650f7a21
Xrq1995 <861995928@qq.com>
2018-06-14 13:50:29 +0800

    Initial commit

---
+++ src/LintCode/DoubleTest.java
@@ -0,0 +1,13 @@
+package LintCode;
+
+/**
+ * @author XuRuiQing
+ * @date 2018/4/7 12:39
+ */
+public class DoubleTest {
+	public static void main(String[] args) {
+		double x=2,y;
+		y=x+3/2;
+		System.out.println(y);
+	}
+}
---
+++ src/LintCode/StaticDispatch.java
@@ -0,0 +1,8 @@
+package LintCode;
+
+/**
+ * @author XuRuiQing
+ * @date 2018/6/14 13:47
+ */
+public class StaticDispatch {
+}
